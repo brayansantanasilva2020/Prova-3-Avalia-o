e6e22ece42978d287e0aa94625b8e63960af5242
brayansantanasilva2020 <brayansantanasilva2020@gmail.com>
2026-09-14 13:07:55 +0000

    v1

---
+++ main.py
@@ -0,0 +1,10 @@
+startup = {
+    "nome": "CyberPulse Tech",
+    "segmento": "Segurança da Informação",
+    "ano_adesao": "2026"
+}
+solucoes_ativas = ["Firewall IA", "Scan de Vulnerabilidades"]
+
+print("CyberPulse Tech:", startup)
+print("Segmento:", startup["segmento"])
+print("Firewall:", solucoes_ativas[0])
